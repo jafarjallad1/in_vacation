5a6914b98b8b4a8bee2dfe7a33c95c31813986a4
jafar jallad <jafarjallad00@gmail.com>
2024-11-20 15:11:27 -0800

    back end with cors

--- src/initapp.js
+++ src/initapp.js
@@ -2,9 +2,11 @@ import connectDb from '../DB/connection.js';
 import authrouter from './modules/auth/auth.router.js';
 import chaletRouter from "./modules/chalet/chalet.router.js";
 import ownerRouter from './modules/owner/owner.router.js';
+import cors from 'cors';
 const initapp = (app,express) => {
     // Middleware for parsing JSON request bodies
     connectDb();
+    app.use(cors());
     app.use(express.json());
     app.use('/auth',authrouter);
     app.use('/chalets',chaletRouter);
